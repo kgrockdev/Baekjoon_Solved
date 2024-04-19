12890f01b081280b477dd521350f8c6ca28113a9
KyungRock Kim <kgrock.dev@gmail.com>
2024-04-20 00:55:00 +0900

    Solved_1094

--- Baekjoon/Private/Problem_1094.cpp
+++ Baekjoon/Private/Problem_1094.cpp
@@ -1,6 +1,27 @@
 #include "Problem_1094.h"
 
+using namespace std;
+
+#include <iostream>
+
 void CProblem_1094::Solve_Problem()
 {
+	int iStick = { };
+	cin >> iStick;
 
+	int iBit = { 0 };
+	int iDetectBit = { 64 };
+
+	while (iDetectBit > 0)
+	{
+		if (iStick - iDetectBit >= 0)
+		{
+			iBit += 1;
+			iStick -= iDetectBit;
+		}
+
+		iDetectBit = iDetectBit >> 1;
+	}
+
+	cout << iBit;
 }
