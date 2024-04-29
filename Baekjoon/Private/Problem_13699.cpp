b05afe814a30ec9818ef820afbefc6bd3111feeb
KyungRock Kim <kgrock.dev@gmail.com>
2024-04-29 18:30:39 +0900

    Saved_13699

---
+++ Baekjoon/Private/Problem_13699.cpp
@@ -0,0 +1,38 @@
+#include "Problem_13699.h"
+
+using namespace std;
+
+#include <iostream>
+#include <unordered_map>
+
+unordered_map<int, unsigned long long>mapContainer13699;
+
+unsigned long long Calc_13699(int iNum)
+{
+    auto iter = mapContainer13699.find(iNum);
+    if (iter != mapContainer13699.end())
+        return iter->second;
+    else
+    {
+        unsigned long long ullSrc = { };
+        for (int iIndex = 0; iIndex < iNum; ++iIndex)
+        {
+
+        }
+
+        mapContainer13699.emplace(iNum, ullSrc);
+        return ullSrc;
+    }
+}
+
+int CProblem_13699::Solve_Problem()
+{
+    mapContainer13699.emplace(0, 1);
+
+    int iNum = { };
+    cin >> iNum;
+
+    cout << Calc_13699(iNum) << '\n';
+
+    return 0;
+}
