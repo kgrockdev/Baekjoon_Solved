add737187a1234573de4172e62b463d2fa61ff1e
KyungRock Kim <kgrock.dev@gmail.com>
2024-07-02 02:31:52 +0900

    Solved_5339

---
+++ Baekjoon/Private/Problem_5339.cpp
@@ -0,0 +1,21 @@
+#include "Problem_5339.h"
+
+#include <iostream>
+
+using namespace std;
+
+int CProblem_5339::Solve_Problem()
+{
+    cout << "     /~\\" << endl;
+    cout << "    ( oo|" << endl;
+    cout << "    _\\=/_" << endl;
+    cout << "   /  _  \\" << endl;
+    cout << "  //|/.\\|\\\\" << endl;
+    cout << " ||  \\ /  ||" << endl;
+    cout << "============" << endl;
+    cout << "|          |" << endl;
+    cout << "|          |" << endl;
+    cout << "|          |" << endl;
+
+    return 0;
+}
